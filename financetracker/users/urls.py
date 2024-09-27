39403544d43f073d97ad8eece8eb9014182ed742
WarLoRDLaRs <sequeiralarsondev@gmail.com>
2024-09-28 01:29:25 +0530

    Fixed Gitignore

--- financetracker/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import user_login, register_user
-
-app_name = 'users'
-urlpatterns = [
-    path('login', user_login, name='login'),
-    path('register', register_user, name='register')
-]
